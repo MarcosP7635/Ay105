0905683756ffe97c070a6d2cafc41813e2bc65b1
MarcosP7635 <marcos.aaron.perez@gmail.com>
2023-05-08 23:01:35 -0700

    going to make a pipeline for each lab

--- error_propagation.py
+++ error_propagation.py
@@ -70,10 +70,9 @@ def general (dict, fun):
             f = fun.diff(symbol_list[index])
             lam_f = lambdify(symbol_list[0:len(keys)], f)
             sum_squares += (lam_f(*keys) * values[index]) ** 2
-            print(sum_squares)
         lam_og = lambdify(symbol_list[0:len(keys)], fun)
         best_est = lam_og(*keys)
-        print(fun)
+        print(sympy.N(fun))
 
     except:
         print("Singular uncertainty entered")
